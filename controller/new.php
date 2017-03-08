d7ae833227669fe8ba3d5403745bd6ddec05e728
zhangy <zhangy@tv189.com>
2017-03-08 21:20:43 +0800

    测试

--- controller/new.php
+++ controller/new.php
@@ -105,12 +105,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			"ListsTime" => $TimeStamp,
 			"Log" => ""
 		);
-
-        print_r($TopicData);
-		$NewTopicResult = $DB->query("INSERT INTO `" . PREFIX . "topics` 
-			(
+        
+		$NewTopicResult = $DB->query("INSERT INTO `" . PREFIX . "topics` (
 				`ID`, 
-				`GroupID`
+				`GroupID`,
 				`Topic`, 
 				`Tags`, 
 				`BoardID`,
@@ -135,9 +133,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				`Lists`, 
 				`ListsTime`, 
 				`Log`
-			) 
-			VALUES 
-			(
+			) VALUES (
 				:ID,
 				:GroupID,
 				:Topic,
