9c49b144201ccad9d433aec9598fd9878349ac92
zhangy <zhangy@tv189.com>
2017-03-09 20:20:56 +0800

    版块详情

---
+++ controller/board.php
@@ -0,0 +1,40 @@
+<?php
+require(LanguagePath . 'board.php');
+$BoardID = intval(Request('Get', 'id'));
+$Page    = Request('Get', 'page');
+$BoardInfo = array();
+
+if ($BoardID)
+	$BoardInfo = $DB->row('SELECT * FROM `' . PREFIX . 'boards`  WHERE ID = ? AND GroupID = ?',
+		array($BoardID, $CurGroupID)
+	);
+if (empty($BoardInfo) || ($BoardInfo['IsEnabled'] == 0 && $CurUserRole < 3))
+{
+    AlertMsg('404 Not Found', '404 Not Found', 404);
+}
+$TotalPage = ceil($BoardInfo['TotalPosts'] / $Config['TopicsPerPage']);
+if ($Page < 1)
+{
+//    Redirect('tag/' . $TagInfo['Name']);
+    $Page = 1;
+}
+else if ($Page > $TotalPage)
+{
+//    Redirect('tag/' . $TagInfo['Name'] . '/page/' . $TotalPage);
+    $Page = $TotalPage;
+}
+
+$TopicsArray = $DB->query("SELECT `ID`, `Topic`, `UserID`, `UserName`, `LastName`, `LastTime`, `Replies` FROM `" . PREFIX . "topics` WHERE 
+BoardID = :BoardID AND IsDel = 0 ORDER BY ID DESC LIMIT :offset,:limit", array(
+    'BoardID' => $BoardID,
+    'offset'    => ($Page - 1) * $Config['TopicsPerPage'],
+    'limit' => $Config['TopicsPerPage']
+));
+
+
+$DB->CloseConnection();
+$PageTitle = $BoardInfo['Name'];
+$PageTitle .= $Page > 1 ? ' Page' . $Page : '';
+$PageMetaDesc = $BoardInfo['Name'] . ' - ' . htmlspecialchars(mb_substr(trim(strip_tags($BoardInfo['Description'])), 0, 150, 'utf-8'));
+$ContentFile  = $TemplatePath . 'board.php';
+include($TemplatePath . 'layout.php');
---
+++ view/mobile/template/board.php
@@ -0,0 +1,64 @@
+<?php
+if (!defined('InternalAccess')) exit('error: 403 Access Denied');
+?>
+<h2 class="expanded" onclick="showHide(this,'TagDescription<?php echo $TagInfo['ID']; ?>');"><?php echo $TagInfo['Name']; ?></h2>
+<p id="TagDescription<?php echo $TagInfo['ID']; ?>">
+<?php
+if($CurUserID){
+?>
+<a href="#" class="button block" onclick="javascript:Manage('<?php echo $TagInfo['ID']; ?>', 4, 2, false, this);"><?php echo $IsFavorite?$Lang['Unfollow']:$Lang['Follow']; ?></a>
+<?php
+}
+echo $TagInfo['Description'];
+?>
+</p>
+<ul class="list topic-list">
+<?php
+if($Page>1){
+?>
+	<li class="pagination"><a href="<?php echo $Config['WebsitePath']; ?>/tag/<?php echo $BoardInfo['ID'].'/page/'.($Page-1); ?>?token=<?php echo $accessToken; ?>" data-transition="slide"><?php echo $Lang['Page_Previous']; ?></a></li>
+
+<?php
+}
+?>
+<!-- main-content start -->
+<?php
+foreach ($TopicsArray as $Topic) {
+?>
+	<li>
+		<div class="avatar">
+			<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo $Topic['UserID']; ?>?token=<?php echo $accessToken; ?>" data-transition="slide">
+					<?php echo GetAvatar($Topic['UserID'], $Topic['UserName'], 'middle'); ?>
+			</a>
+		</div>
+		<div class="content">
+			<a href="<?php echo $Config['WebsitePath']; ?>/t/<?php echo $Topic['ID']; ?>" data-transition="slide">
+				<h2><?php echo $Topic['Topic']; ?></h2>
+			</a>
+			<p><?php echo FormatTime($Topic['PostTime']); ?>&nbsp;&nbsp;<?php echo htmlspecialchars($Topic['UserName']); ?></p>
+
+			<span class="aside">
+				<?php echo $Topic['Replies']; ?>
+			</span>回帖数
+
+		</div>
+		
+		<div class="c"></div>
+	</li>
+<?php
+} 
+if($Page<$TotalPage){
+?>
+	<li class="pagination"><a href="<?php echo $Config['WebsitePath']; ?>/tag/<?php echo $BoardInfo['ID'] .'/page/'.($Page+1); ?>" data-transition="slide" data-persist-ajax="false"><?php echo $Lang['Page_Next']; ?></a></li>
+<?php } ?>
+
+</ul>
+<ul class="list">
+	<li class="divider"><?php echo $Lang['Tag']; ?>：<?php echo $TagName; ?></li>
+	<li>
+		共<?php echo $BoardInfo['TotalPosts']; ?><?php echo $Lang['Topics']; ?>
+		今天<?php echo $BoardInfo['TodayPosts']; ?><?php echo $Lang['Topics']; ?>
+	</li>
+	<li><?php echo $Lang['Created_In']; ?><?php echo FormatTime($BoardInfo['DateCreated']); ?></li>
+	<li><?php echo $Lang['Last_Updated_In']; ?><?php echo FormatTime($BoardInfo['MostRecentPostTime']); ?></li>
+</ul>
