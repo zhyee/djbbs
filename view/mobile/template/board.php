8b091a53823638e4b026839cfe05a90559ace7fb
zhangy <zhangy@tv189.com>
2017-03-09 21:07:07 +0800

    版块详情

--- view/mobile/template/board.php
+++ view/mobile/template/board.php
@@ -26,11 +26,10 @@ foreach ($TopicsArray as $Topic) {
 				<h2><?php echo $Topic['Topic']; ?></h2>
 			</a>
 			<p>
-				<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo $Topic['UserID']; ?>?token=<?php echo $accessToken; ?>" data-transition="slide"><?php echo htmlspecialchars($Topic['UserName']); ?></a> 发表于 <?php echo FormatTime($Topic['PostTime']); ?></p>
+				<a href="<?php echo $Config['WebsitePath']; ?>/u/<?php echo $Topic['UserID']; ?>?token=<?php echo $accessToken; ?>" data-transition="slide"><?php echo htmlspecialchars($Topic['UserName']); ?></a> 发表于 <?php echo FormatTime($Topic['PostTime']); ?>
+			</p>
 
-			<span class="aside">
-				<?php echo $Topic['Replies']; ?>
-			</span> &nbsp;&nbsp;回帖数
+			<p><span class="aside"><?php echo $Topic['Replies']; ?></span> &nbsp;&nbsp;回帖数</p>
 
 		</div>
 		
@@ -46,9 +45,7 @@ if($Page<$TotalPage){
 </ul>
 <ul class="list">
 	<li class="divider"><?php echo $Lang['Board']; ?>：<?php echo htmlspecialchars($BoardInfo['Name']); ?></li>
-	<li>
-		共 <?php echo $BoardInfo['TodayPosts']; ?> / <?php echo $BoardInfo['TotalPosts']; ?> <?php echo $Lang['Topics']; ?>
-	</li>
+	<li>共 <?php echo $BoardInfo['TodayPosts']; ?> / <?php echo $BoardInfo['TotalPosts']; ?> <?php echo $Lang['Topics']; ?></li>
 	<li><?php echo $Lang['Created_In']; ?><?php echo FormatTime($BoardInfo['DateCreated']); ?></li>
 	<li><?php echo $Lang['Last_Updated_In']; ?><?php echo FormatTime($BoardInfo['MostRecentPostTime']); ?></li>
 </ul>
