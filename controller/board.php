8a0de47ce26b040f51f8183d9c1c577845fee6be
zhangy <zhangy@tv189.com>
2017-03-11 14:53:22 +0800

    修改样式

--- controller/board.php
+++ controller/board.php
@@ -1,20 +1,37 @@
 <?php
 require(LanguagePath . 'board.php');
 $BoardID = (int)Request('Get', 'id');
-$type    = (int)Request('Get', 'type');
+$type    = (int)Request('Get', 'type');     // 0全部  / 1与我相关
 $Page    = (int)Request('Get', 'page');
 
 $BoardInfo = array();
 
 if ($BoardID)
-	$BoardInfo = $DB->row('SELECT * FROM `' . PREFIX . 'boards`  WHERE ID = ? AND GroupID = ?',
-		array($BoardID, $CurGroupID)
-	);
+{
+    $BoardInfo = $DB->row('SELECT * FROM `' . PREFIX . 'boards`  WHERE ID = ? AND GroupID = ?',
+        array($BoardID, $CurGroupID)
+    );
+}
 if (empty($BoardInfo) || ($BoardInfo['IsEnabled'] == 0 && $CurUserRole < 3))
 {
     AlertMsg('404 Not Found', '404 Not Found', 404);
 }
-$TotalPage = ceil($BoardInfo['TotalPosts'] / $Config['TopicsPerPage']);
+
+if ($type == 1)
+{
+    $TotalPosts = $DB->single("SELECT COUNT(DISTINCT TopicID) FORM `" . PREFIX . "posts` WHERE UserID = ? AND IsDel = ?",
+        array($CurUserID, 0));
+
+    var_dump($TotalPosts);
+    die;
+
+    $TotalPage = ceil($TotalPosts / $Config['TopicsPerPage']);
+}
+else
+{
+    $TotalPage = ceil($BoardInfo['TotalPosts'] / $Config['TopicsPerPage']);
+}
+
 if ($Page < 1)
 {
 //    Redirect('tag/' . $TagInfo['Name']);
@@ -25,6 +42,9 @@ else if ($Page > $TotalPage)
 //    Redirect('tag/' . $TagInfo['Name'] . '/page/' . $TotalPage);
     $Page = $TotalPage;
 }
+
+
+
 
 $TopicsArray = $DB->query("SELECT `ID`, `Topic`, `UserID`, `UserName`, `PostTime`, `LastName`, `LastTime`, `Replies` FROM `" . PREFIX . "topics` WHERE 
 BoardID = :BoardID AND IsDel = 0 ORDER BY ID DESC LIMIT :offset,:limit", array(
