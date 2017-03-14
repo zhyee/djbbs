127b29e92f9c47ac4fe5ca57d9e71d80cf04919e
zhangy <zhangy@tv189.com>
2017-03-14 15:39:47 +0800

    优化样式

--- view/mobile/template/board.php
+++ view/mobile/template/board.php
@@ -52,3 +52,14 @@ if($Page<$TotalPage){
 <?php } ?>
 
 </ul>
+
+<script type="text/javascript">
+	$(".board-list>li").click(function (e) {
+		if (e.target.tagName.toUpperCase() === 'A')
+		{
+			return;
+		}
+		location.href = $(this).find("a:first").attr("href");
+	});
+
+</script>
